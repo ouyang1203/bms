8a42b1d2a5fbe5945aa05f7e99c48bf7ace36683
ouyang1203 <ouyanghui1203@163.com>
2020-03-15 11:53:03 +0800

    调整各模块配置文件

--- bmsDaily/src/main/java/com/pccw/kernel/bmsDaily/DailyApplication.java
+++ bmsDaily/src/main/java/com/pccw/kernel/bmsDaily/DailyApplication.java
@@ -4,13 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
- * 禁用spring security
+ * 禁用spring security,暂时不连接数据源
  * */
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,DataSourceAutoConfiguration.class })
 @EnableFeignClients
 public class DailyApplication {
 	private static Logger log_ = LoggerFactory.getLogger(DailyApplication.class);
