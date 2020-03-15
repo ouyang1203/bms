de536ac680e88e2cf296fe3572bef91f7a60c2b2
ouyang1203 <ouyanghui1203@163.com>
2020-03-15 12:22:25 +0800

    修改

--- bmsAnnual/src/main/java/com/pccw/kernel/bmsAnnual/AnnualApplication.java
+++ bmsAnnual/src/main/java/com/pccw/kernel/bmsAnnual/AnnualApplication.java
@@ -4,10 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,DataSourceAutoConfiguration.class })
 @EnableFeignClients
 public class AnnualApplication {
 	private static Logger log_ = LoggerFactory.getLogger(AnnualApplication.class);
--- bmsEurzka/src/main/java/com/pccw/kernel/bmsEurzka/EurzkaApplication.java
+++ bmsEurzka/src/main/java/com/pccw/kernel/bmsEurzka/EurzkaApplication.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 public class EurzkaApplication {
 	private static Logger log_ = LoggerFactory.getLogger(EurzkaApplication.class);
